Fixed masking for wn model. Now antonym loss goes to close to zero.

model.py
@@ -188,11 +188,12 @@ class RNNModel(nn.Module):
             return weight.new_zeros(self.nlayers, bsz, self.nhid)
 
 class WNModel(nn.Module):
-    def __init__(self, embedding, emb_dim, wn_dim, antonym_margin=1, dist_fn=F.pairwise_distance, fixed=False, random=False):
+    def __init__(self, embedding, emb_dim, wn_dim, pad_idx, antonym_margin=1, dist_fn=F.pairwise_distance, fixed=False, random=False):
         super(WNModel, self).__init__()
         self.embedding = embedding
         self.emb_dim = emb_dim
         self.wn_dim = wn_dim
+        self.pad_idx = pad_idx
 
         self.syn_proj = nn.Linear(emb_dim, wn_dim, bias=False)
         self.hypn_proj = nn.Linear(emb_dim, wn_dim, bias=False)
@@ -231,27 +232,35 @@ class WNModel(nn.Module):
         if synonyms is not None:
             emb_syn1 = self.syn_proj(self.embedding(synonyms[:, 0]))
             emb_syn2 = self.syn_proj(self.embedding(synonyms[:, 1]))
-            output_dict['loss_syn'] = torch.mean(self.dist_fn(emb_syn1, emb_syn2))
+            syn_mask = 1 - (synonyms[:,0] == self.pad_idx).float()
+            syn_len = torch.sum(syn_mask)
+            output_dict['loss_syn'] = torch.sum(self.dist_fn(emb_syn1, emb_syn2) * syn_mask)/syn_len
             output_dict['syn_emb'] = (emb_syn1, emb_syn2)
 
         if antonyms is not None:
             emb_ant1 =self.syn_proj(self.embedding(antonyms[:, 0]))
             emb_ant2 = self.syn_proj(self.embedding(antonyms[:, 1]))
-            output_dict['loss_ant'] = torch.mean(F.relu(self.antonym_margin - self.dist_fn(emb_ant1, emb_ant2)))
+            ant_mask = 1 - (antonyms[:,0] == self.pad_idx).float()
+            ant_len = torch.sum(ant_mask)
+            output_dict['loss_ant'] = torch.sum(F.relu(self.antonym_margin - self.dist_fn(emb_ant1, emb_ant2)) * ant_mask)/ant_len
             output_dict['ant_emb'] = (emb_ant1, emb_ant2)
 
         if hypernyms is not None:
             emb_hypn1 = self.hypn_proj(self.embedding(hypernyms[:, 0]))
             emb_hypn2 = self.hypn_proj(self.embedding(hypernyms[:, 1]))
             emb_hypn1 = self.hypn_rel(emb_hypn1)
-            output_dict['loss_hyp'] = torch.mean(self.dist_fn(emb_hypn1, emb_hypn2))
+            hyp_mask = 1 - (hypernyms[:,0] == self.pad_idx).float()
+            hyp_len = torch.sum(hyp_mask)
+            output_dict['loss_hyp'] = torch.sum(self.dist_fn(emb_hypn1, emb_hypn2) * hyp_mask)/hyp_len
             output_dict['hyp_emb'] = (emb_hypn1, emb_hypn2)
 
         if meronyms is not None:
             emb_mern1 = self.mern_proj(self.embedding(meronyms[:, 0]))
             emb_mern2 = self.mern_proj(self.embedding(meronyms[:, 1]))
             emb_mern1 = self.mern_rel(emb_mern1)
-            output_dict['loss_mer'] = torch.mean(self.dist_fn(emb_mern1, emb_mern2))
+            mer_mask = 1 - (meronyms[:,0] == self.pad_idx).float()
+            mer_len = torch.sum(mer_mask)
+            output_dict['loss_mer'] = torch.sum(self.dist_fn(emb_mern1, emb_mern2) * mer_mask)/mer_len
             output_dict['mer_emb'] = (emb_mern1, emb_mern2)
 
         return output_dict
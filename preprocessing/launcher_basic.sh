0e20eef44b580fb523eb56b9ff2bf474105321af
Kushal Arora <kushal18@gmail.com>
2019-02-25 05:17:08 -0800

    we don't need 8 cpus for building preprocessed file.

--- preprocessing/launcher_basic.sh
+++ preprocessing/launcher_basic.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH --account=def-dprecup
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
 #SBATCH --mem=120000M
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kushal.arora@mail.mcgill.ca

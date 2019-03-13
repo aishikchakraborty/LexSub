df4a66bcf2b045a409f05055cfa5e667b217f443
Kushal Arora <kushal18@gmail.com>
2019-03-13 05:13:35 -0400

    Added support for printing hypernymysuite results.

--- scripts/parse_output.py
+++ scripts/parse_output.py
@@ -58,13 +58,31 @@ with open('lm_wn_machine_assignments%s.txt' % ('-v2' if version=='v2' else ''), 
                         output[ws_bench] = '%.4f' % (score)
                         ws_bench = None
 
-            for ext, key in [('ner', 'test_f1-measure-overall'), ('sst', 'test_accuracy'), ('esim', 'test_accuracy'),('bidaf', 'best_validation_em')]:
+            for ext, key in [('ner', 'test_f1-measure-overall'),
+                                ('sst', 'test_accuracy'),
+                                # ('esim', 'test_accuracy'),
+                                ('decomposable', 'test_accuracy'),
+                                ('bidaf', 'best_validation_em')]:
                 ext_path = os.path.join(path, ext+'/metrics.json')
                 if os.path.exists(ext_path):
                     with open(ext_path) as ext_stdout:
                         obj = json.load(ext_stdout)
                         output[ext] = '%.4f' % float(obj[key])
 
+            for name, prefix in [('hypernymysuite', ''), ('hyp_hypernymysuite', 'hyp_'), ('syn_hypernymysuite', 'syn_'), ('mer_hypernymysuite', 'mer_')]:
+                ext_path = os.path.join(path, '%s.json' % name)
+                if os.path.exists(ext_path):
+                    with open(ext_path) as ext_stdout:
+                        obj = json.load(ext_stdout)
+                        output[prefix + 'dir_wbless'] = '%.4f' % obj['dir_wbless']['acc_test_inv']
+                        output[prefix + 'dir_bibless'] = '%.4f' % obj['dir_bibless']['acc_test_inv']
+                        output[prefix + 'dir_dbless'] = '%.4f' % obj['dir_dbless']['acc_test']
+                        output[prefix + 'cor_hyperlex'] = '%.4f' % obj['cor_hyperlex']['rho_all']
+                        output[prefix + 'siege_bless'] = '%.4f' % obj['siege_bless']['other']['ap_test']
+                        output[prefix + 'siege_leds'] = '%.4f' % obj['siege_leds']['other']['ap_test']
+                        output[prefix + 'siege_eval'] = '%.4f' % obj['siege_eval']['other']['ap_test']
+                        output[prefix + 'siege_weeds'] = '%.4f' % obj['siege_weeds']['other']['ap_test']
+                        output[prefix + 'siege_shwartz'] = '%.4f' % obj['siege_shwartz']['other']['ap_test']
             outputs.append(output)
 
         except Exception as e:
@@ -75,7 +93,11 @@ with open('lm_wn_machine_assignments%s.txt' % ('-v2' if version=='v2' else ''), 
                 'syn_men3k', 'syn_simlex999', 'syn_simverb3500', 'syn_wordsim353_relatedness', 'syn_hyperlex', 'syn_hyperlex-nouns', 'syn_hyperlex_test', \
                 'hyp_men3k', 'hyp_simlex999', 'hyp_simverb3500', 'hyp_wordsim353_relatedness', 'hyp_hyperlex', 'hyp_hyperlex-nouns', 'hyp_hyperlex_test', \
                 'mer_men3k', 'mer_simlex999', 'mer_simverb3500', 'mer_wordsim353_relatedness', 'mer_hyperlex', 'mer_hyperlex-nouns', 'mer_hyperlex_test', \
-                'ner', 'sst', 'esim', 'bidaf']
+                'dir_wbless', 'dir_bibless', 'dir_dbless', 'cor_hyperlex', 'siege_bless', 'siege_leds', 'siege_eval', 'siege_weeds', 'siege_shwartz', \
+                'syn_dir_wbless', 'syn_dir_bibless', 'syn_dir_dbless', 'syn_cor_hyperlex', 'syn_siege_bless', 'syn_siege_leds', 'syn_siege_eval', 'syn_siege_weeds', 'syn_siege_shwartz', \
+                'hyp_dir_wbless', 'hyp_dir_bibless', 'hyp_dir_dbless', 'hyp_cor_hyperlex', 'hyp_siege_bless', 'hyp_siege_leds', 'hyp_siege_eval', 'hyp_siege_weeds', 'hyp_siege_shwartz', \
+                'mer_dir_wbless', 'mer_dir_bibless', 'mer_dir_dbless', 'mer_cor_hyperlex', 'mer_siege_bless', 'mer_siege_leds', 'mer_siege_eval', 'mer_siege_weeds', 'mer_siege_shwartz', \
+                'ner', 'sst', 'decomposable', 'bidaf']
     output_csv.write('%s\n' % ','.join(fields))
     for output in outputs:
         print(output)
